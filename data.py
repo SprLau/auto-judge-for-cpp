5144202623ba3f2dc46774257662ef95f0016356
SprLau <lau@buaa.edu.cn>
2021-09-02 01:20:35 +0800

    refine print_to_file()

--- data.py
+++ data.py
@@ -2,8 +2,8 @@ import random
 
 txt_file = open("data.txt", "w+")
 
-def print_to_file(something):
-    print(something, end="", file=txt_file)
+def print_to_file(something, end):
+    print(something, end=end, file=txt_file)
 
 #####################################################################
 # Here to build your data generator,                                # 
